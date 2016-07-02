b331b7cd87fd29704be6a16a95bacbf50e2e021c
Justin Hill <contact@justin-hill.com>
2016-07-02 15:30:29 -0700

    basic circles

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,25 @@
-var dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19,
-                14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
-                24, 18, 25, 9, 3 ];
+var dataset = [ 5, 10, 15, 20, 25 ];
+var w = 500;
+var h = 50;
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
 
-d3.select("body").selectAll("div")
-    .data(dataset)
-    .enter()
-    .append("div")
-    .attr("class", "bar")
-    .style("height", function(d) {
-    var barHeight = d * 5;  //Scale up by factor of 5
-    return barHeight + "px";
-});
+var circles = svg.selectAll("circle")
+                 .data(dataset)
+                 .enter()
+                 .append("circle");
+
+circles.attr("cx", function(d, i) {
+           return (i * 50) + 25;
+       })
+      .attr("cy", h/2)
+      .attr("r", function(d) {
+           return d;
+      })
+      .attr("fill", "yellow")
+      .attr("stroke", "orange")
+      .attr("stroke-width", function(d) {
+          return d/2;
+      });
