d8ac94fe928d8ee0c7122a3d92fd5e0e442af1fb
Justin Hill <contact@justin-hill.com>
2016-07-04 17:40:22 -0700

    pie chart

--- src/js/app.js
+++ src/js/app.js
@@ -107,3 +107,43 @@ d3.select("p")
 
     });
 }
+
+//piechart
+
+function pieChart() {
+  var dataset = [ 5, 10, 20, 45, 6, 25 ];
+  var pie = d3.pie();
+  var w = 300;
+  var h = 300;
+  var outerRadius = w / 2;
+  var innerRadius = 0;
+  var color = d3.schemeCategory10;
+  var arc = d3.arc()
+                  .innerRadius(innerRadius)
+                  .outerRadius(outerRadius);
+  var svg = d3.select("body")
+              .append("svg")
+              .attr("width", w)
+              .attr("height", h);
+  var arcs = svg.selectAll("g.arc")
+          .data(pie(dataset))
+          .enter()
+          .append("g")
+          .attr("class", "arc")
+          .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")");
+  arcs.append("path")
+    .attr("fill", function(d, i) {
+        return color[i];
+    })
+    .attr("d", arc);
+
+  arcs.append("text")
+    .attr("transform", function(d) {
+        return "translate(" + arc.centroid(d) + ")";
+    })
+    .attr("text-anchor", "middle")
+    .text(function(d) {
+        return d.value;
+    });
+
+}
